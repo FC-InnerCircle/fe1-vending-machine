25d5ff74f0aa2d1763c36f35c9f144eecdd36dc3
silveriron <kichul7493@gmail.com>
2024-07-20 20:54:30 +0900

    chore: 프로젝트 초기 세팅

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"), // contentBase를 static으로 변경
+    compress: true,
+    port: 9000,
+    open: true, // 브라우저 자동 열기
+    hot: true, // 모듈 핫 리플레이스먼트 활성화,
+  },
+};
