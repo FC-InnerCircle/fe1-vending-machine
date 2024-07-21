e91787ee178fba33b59e14e179f547dc0a57fba8
wpdbsx <wpdbsx@naver.com>
2024-07-22 01:22:57 +0900

    feat 버튼 hover 기능 구현 및 퍼블리싱 작업

--- public/js/render.js
+++ public/js/render.js
@@ -1,6 +1,25 @@
 const render = (function () {
     function update(state) {
-        document.getElementById('count').textContent = state.count;
+        // document.getElementById('count').textContent = state.count;
+
+        const itemList = document.getElementById('itemList');
+        console.log("itemList:", itemList)
+        itemList.innerHTML = state.items.map(item => `
+            <button class="item-button p-2 bg-red-100 hover:bg-red-300 m-2 rounded" data-id="${item.id}">
+              ${item.text}
+            </button>
+          `).join('');
+
+        document.querySelectorAll('.item-button').forEach(button => {
+            button.addEventListener('click', function () {
+                button.classList.add('hover:bg-red-500');
+            });
+            button.addEventListener('mouseleave', function () {
+                button.classList.remove('hover:bg-red-500');
+            });
+        });
+
+
     }
 
     return {
--- public/js/state.js
+++ public/js/state.js
@@ -1,6 +1,18 @@
 const state = (function () {
     let state = {
-        count: 0
+        count: 0,
+        items: [
+            { id: 1, text: 'FE300', price: 300 },
+            { id: 2, text: 'FE600', price: 600 },
+            { id: 3, text: 'FE1200', price: 1200 },
+            { id: 4, text: 'FE1500', price: 1500 },
+            { id: 5, text: 'FE1800', price: 1800 },
+            { id: 6, text: 'FE2100', price: 2100 },
+            { id: 7, text: 'FE2200', price: 2200 },
+            { id: 8, text: 'FE2400', price: 2400 },
+            { id: 9, text: 'FE2500', price: 2500 }
+        ]
+
     };
 
     const listeners = [];
