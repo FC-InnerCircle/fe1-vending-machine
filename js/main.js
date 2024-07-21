c39a4687416efad444a758de82ab406e457f85ea
yoo94 <ahy0119@naver.com>
2024-07-21 16:21:49 +0900

    -금액 반환 기능 구현

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const inputDisplay = document.querySelector(".input-display");
     const messageBox = document.querySelector(".message-box");
-    const screenInput = document.querySelector(".screen");
+    const screenInput = document.getElementById("screen");
     const insertButton = document.getElementById("insert");
+    const refundButton = document.getElementById("refund");
     let totalInserted = 0;
+
     function formatCurrency(value) {
         return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
@@ -13,6 +15,10 @@ document.addEventListener("DOMContentLoaded", () => {
     function updateScreen() {
         screenInput.value = formatCurrency(totalInserted);
     }
+    function updateMesaageBox(inputText) {
+        messageBox.value += `\n${inputText}`;
+    }
+
     inputDisplay.addEventListener("input", () => {
         let value = inputDisplay.value.replace(/,/g, ''); // Remove existing commas
         if (!isNaN(value) && value !== "") {
@@ -23,9 +29,19 @@ document.addEventListener("DOMContentLoaded", () => {
         const amount = unFormatCurrency(inputDisplay.value);
         if (amount > 0) {
             totalInserted += amount;
-            messageBox.value += `\n${formatCurrency(amount)}원을 투입했습니다.`;
+            updateMesaageBox(`${formatCurrency(amount)}원을 투입했습니다.`);
             updateScreen();
             inputDisplay.value = "";
+        }
+    });
+    refundButton.addEventListener("click", () => {
+        if (totalInserted > 0) {
+            updateMesaageBox(`${formatCurrency(totalInserted)}원을 반환했습니다.`);
+            totalInserted = 0;
+            updateScreen();
+            inputDisplay.value = "";
+        }else {
+            messageBox.value += `\n반환할 금액이 없습니다.`;
         }
     });
 });
