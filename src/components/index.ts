665263e6efab945242f5c7176f6a7c2bbfa19c5c
silveriron <kichul7493@gmail.com>
2024-07-22 22:10:24 +0900

    feat: 컴포넌트 export index 파일 생성

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from "./Button/Button";
+export { default as Input } from "./Input/Input";
+export { default as Main } from "./Main/Main";
+export { default as Section } from "./Section/Section";
+export { default as Div } from "./Div/Div";
