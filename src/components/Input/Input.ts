347f417c679ff12efe77d33a359aaf3287d55561
silveriron <kichul7493@gmail.com>
2024-07-22 22:07:52 +0900

    feat: input 컴포넌트 클래스 생성

---
+++ src/components/Input/Input.ts
@@ -0,0 +1,26 @@
+import BaseComponent, { ComponentProps } from "../BaseComponent/BaseComponent";
+
+type InputProps = ComponentProps & {
+  type?: string;
+  placeholder?: string | number;
+  value?: string | number;
+  onChange?: (e: Event) => void;
+};
+
+export default class Input extends BaseComponent {
+  constructor(props: InputProps = {}) {
+    super("input", props);
+  }
+
+  render(): void {
+    this.element.addEventListener("input", this.props.onChange);
+    this.element.setAttribute("type", this.props.type || "text");
+    this.element.setAttribute("placeholder", this.props.placeholder as string);
+    this.element.setAttribute("value", this.props.value as string);
+    this.element.setAttribute("min", this.props.min as string);
+  }
+
+  resetValue() {
+    (this.element as HTMLInputElement).value = "";
+  }
+}
