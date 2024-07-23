51827f30212ffb49b2607f829e5b080b63351047
2scent <ad7549@gmail.com>
2024-07-23 22:32:40 +0900

    Log 컴포넌트 추가 및 렌더링

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,35 @@
 import Component from "./core/component.ts";
 import Amount from "./components/amount.ts";
+import Log from "./components/log.ts";
 
-export default class App extends Component {
+interface State {
+  logs: Array<string>;
+}
+
+export default class App extends Component<{}, State> {
+  protected initializeState(): State {
+    return {
+      logs: ['로그', '로그', '로그', '로그', '로그', '로그', '로그', '로그', '로그', '로그', '로그'],
+    };
+  }
+
   template () {
     return `
       <div>
         <h1>Vending Machine</h1>
         <div class="amount"></div>
+        <div class="log"></div>
       </div>
     `;
   }
 
   protected mounted() {
     const amountElement = document.querySelector('.amount') as HTMLDivElement;
+    const logElement = document.querySelector('.log') as HTMLDivElement;
+
+    const {logs} = this.state;
 
     new Amount(amountElement, {});
+    new Log(logElement, {logs});
   }
 }
