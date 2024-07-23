13d1ddd506e16462f67a01a2611ced096ccb7098
DahnBee Shim <colleksql@8snippet.com>
2024-07-23 21:30:04 +0900

    pinia app.vue에 연결

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtPage />
 </template>
+
+<script setup>
+import { createPinia } from 'pinia'
+
+// Pinia 초기화
+const pinia = createPinia()
+
+// Nuxt 애플리케이션에 Pinia 추가
+defineNuxtPlugin(nuxtApp => {
+  nuxtApp.vueApp.use(pinia)
+})
+
+// 페이지 헤드 설정
+useHead({
+  title: 'Nuxt3 Vending Machine',
+})
+</script>
