4c212735ed03875ef5d109d1b9f4d667cb61af47
DahnBee Shim <colleksql@8snippet.com>
2024-07-23 21:28:32 +0900

    log가 찍히는 container 작업

---
+++ components/LogContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex-1 w-full p-2 overflow-y-auto border rounded h-44">
+    <ul>
+      <li v-for="(log, index) in logs" :key="index" class="text-black">{{ log }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useVendingMachineStore } from '@/stores/vendingMachine';
+
+const store = useVendingMachineStore();
+const { logs } = storeToRefs(store);
+</script>
