f8f4034c31f00ac08985742ea7d7fb03ffcd0fa3
DahnBee Shim <colleksql@8snippet.com>
2024-07-23 21:27:56 +0900

    자판기 버튼 UI 작업

---
+++ components/ItemButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button 
+    :class="['item', 'bg-blue-200', 'p-4', 'rounded', 'hover:bg-blue-300', 'active:bg-blue-400']" 
+    @click="handleClick"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  price: {
+    type: Number,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['purchase']);
+
+const handleClick = () => {
+  emit('purchase', props.price, props.label);
+};
+</script>
