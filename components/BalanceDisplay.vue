6c99dda4798835435e9e37e8725de612d7f7ddbe
DahnBee Shim <colleksql@8snippet.com>
2024-07-23 21:27:35 +0900

    가격 정보 디스플레이 UI 작업

---
+++ components/BalanceDisplay.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="py-2 mb-2 text-xl text-center bg-gray-300 rounded">
+    {{ balance.toLocaleString() }} 원
+  </div>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useVendingMachineStore } from '@/stores/vendingMachine'
+
+const store = useVendingMachineStore()
+const { balance } = storeToRefs(store)
+</script>
