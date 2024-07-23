c157949e7dcfc31a7fee3b42728ea4c4cdfeed22
DahnBee Shim <colleksql@8snippet.com>
2024-07-23 21:26:53 +0900

    라이브러리 및 모듈 설치

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  modules: ['@nuxt/ui'],
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
+  plugins: [
+    '~/plugins/pinia-plugin-persistedstate.client.js',
+  ],
   devtools: { enabled: true },
 })
