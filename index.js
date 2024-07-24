9c18f2dfa9ab1b4923343e0a1b12dde41354539a
Gentlee-dev <gentlee.dev@gmail.com>
2024-07-24 16:48:44 +0900

    feat : 투입기능 및 잔액 컴마찍기

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const $controllerInput = document.querySelector(".controller_input");
+const $controllerForm = document.querySelector(".controller");
+const $balance = document.querySelector(".balance");
+
+let balance = 0;
+
+const addComma = (number) =>
+  number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 const onChangeControllerInput = (inputEvent) => {
-  const val = inputEvent.target.value;
-  const val2 = val.replace(/[^0-9]/g, "");
-  $controllerInput.value = val2;
+  const valueOnlyNumber = inputEvent.target.value.replace(/[^0-9]/g, "");
+  $controllerInput.value = valueOnlyNumber;
 };
 
 $controllerInput.addEventListener("input", onChangeControllerInput);
+
+$controllerForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  balance += +$controllerInput.value;
+  $balance.value = balance.toLocaleString();
+  $controllerInput.value = "";
+});
