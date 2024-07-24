3e0d876506ff804498ebfc968376cc1f12fb4046
Gentlee-dev <gentlee.dev@gmail.com>
2024-07-24 17:30:23 +0900

    feat : 상품 구매기능 구현

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 const $controllerInput = document.querySelector(".controller_input");
 const $controllerForm = document.querySelector(".controller");
 const $balance = document.querySelector(".balance");
+const $btnWrap = document.querySelector(".btn_wrap");
 
+const CHEAPEST_PRICE = 300;
+const ALL_ITEMS_COUNT = 9;
+const GT = "GT";
 let balance = 0;
 
 const addComma = (number) =>
   number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+const changeBalance = (balance) => {
+  $balance.value = balance;
+};
 
 const onChangeControllerInput = (inputEvent) => {
   const valueOnlyNumber = inputEvent.target.value.replace(/[^0-9]/g, "");
   $controllerInput.value = valueOnlyNumber;
 };
-
-$controllerInput.addEventListener("input", onChangeControllerInput);
-
-$controllerForm.addEventListener("submit", (e) => {
+const onSubmitControllerForm = (e) => {
   e.preventDefault();
   balance += +$controllerInput.value;
-  $balance.value = balance.toLocaleString();
+  changeBalance(balance.toLocaleString());
   $controllerInput.value = "";
-});
+};
+const onClickItem = (price) => {
+  if (balance < price) return;
+
+  balance -= price;
+  changeBalance(balance);
+};
+const makeBtns = () => {
+  for (let i = 0; i < ALL_ITEMS_COUNT; i++) {
+    const btn = document.createElement("button");
+    const price = CHEAPEST_PRICE + i * 100;
+    btn.classList.add("item");
+    btn.innerHTML = `${GT}${price}`;
+    btn.addEventListener("click", () => onClickItem(price));
+
+    $btnWrap.appendChild(btn);
+  }
+};
+
+$controllerInput.addEventListener("input", onChangeControllerInput);
+$controllerForm.addEventListener("submit", onSubmitControllerForm);
+makeBtns();
