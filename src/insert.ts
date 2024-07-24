c57d4ceb3df600d4a0cc88ac5b2ea716ef218cca
AyoungWon <wayinwayne@gmail.com>
2024-07-24 16:55:56 +0900

    feat: input에 숫자 양수만 입력하도록 수정

--- src/insert.ts
+++ src/insert.ts
@@ -1,9 +1,26 @@
-const INIT_VALUE = "0";
+const INIT_VALUE = "";
 
 export class InsertAmountInput {
   private inputElement: HTMLInputElement;
   private initialValue: string;
+  private preventNonNumericInput(event: KeyboardEvent) {
+    const key = event.key;
+    if (
+      key === "Backspace" ||
+      key === "Delete" ||
+      key === "ArrowLeft" ||
+      key === "ArrowRight" ||
+      key === "Tab" ||
+      key === "Escape" ||
+      key === "Enter"
+    )
+      return;
 
+    // Prevent non-numeric input
+    if (key < "0" || key > "9") {
+      event.preventDefault();
+    }
+  }
   constructor() {
     this.inputElement = document.getElementById(
       "amount-input"
@@ -12,6 +29,7 @@ export class InsertAmountInput {
       throw new Error(`Element with id amount-input not found`);
     }
     this.initialValue = INIT_VALUE;
+    this.inputElement.addEventListener("keydown", this.preventNonNumericInput);
   }
 
   getInsertAmount() {
