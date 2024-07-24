609caf3275be9650998f444671f618eaf34f4c7c
Gentlee-dev <gentlee.dev@gmail.com>
2024-07-24 15:29:46 +0900

    feat : 금액 양수만 입력

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log(1);
+const $controllerInput = document.querySelector(".controller_input");
+
+const onChangeControllerInput = (inputEvent) => {
+  const val = inputEvent.target.value;
+  const val2 = val.replace(/[^0-9]/g, "");
+  $controllerInput.value = val2;
+};
+
+$controllerInput.addEventListener("input", onChangeControllerInput);
