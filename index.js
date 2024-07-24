a27d8b5f888c7e66df7f96a880a7ad8bae4b0cf4
Gentlee-dev <gentlee.dev@gmail.com>
2024-07-24 17:37:46 +0900

    feat : 반환기능 구현 및 구매 후 잔액이 최저가보다 적으면 자동반환

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const $controllerInput = document.querySelector(".controller_input");
 const $controllerForm = document.querySelector(".controller");
 const $balance = document.querySelector(".balance");
 const $btnWrap = document.querySelector(".btn_wrap");
+const $returnBalanceBtn = document.querySelector(".return_balance");
 
 const CHEAPEST_PRICE = 300;
 const ALL_ITEMS_COUNT = 9;
-const GT = "GT";
+const GENTLEE = "GT";
 let balance = 0;
 
 const addComma = (number) =>
   number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 const changeBalance = (balance) => {
   $balance.value = balance;
+};
+const returnBalance = () => {
+  balance = 0;
+  changeBalance(balance);
 };
 
 const onChangeControllerInput = (inputEvent) => {
@@ -29,13 +34,14 @@ const onClickItem = (price) => {
 
   balance -= price;
   changeBalance(balance);
+  if (balance < CHEAPEST_PRICE) returnBalance();
 };
 const makeBtns = () => {
   for (let i = 0; i < ALL_ITEMS_COUNT; i++) {
     const btn = document.createElement("button");
     const price = CHEAPEST_PRICE + i * 100;
     btn.classList.add("item");
-    btn.innerHTML = `${GT}${price}`;
+    btn.innerHTML = `${GENTLEE}${price}`;
     btn.addEventListener("click", () => onClickItem(price));
     btn.addEventListener("mousedown", () => {
       balance < price && changeBalance(price);
@@ -52,4 +58,5 @@ const makeBtns = () => {
 
 $controllerInput.addEventListener("input", onChangeControllerInput);
 $controllerForm.addEventListener("submit", onSubmitControllerForm);
+$returnBalanceBtn.addEventListener("click", returnBalance);
 makeBtns();
