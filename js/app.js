ad237ad4fcaefc96d395271178888a19f052da05
kmoh <kyomin0306@gmail.com>
2024-07-24 21:03:23 +0900

    feat: 투입 기능 구현

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
 let amount = 0
 
 function insertMoney() {
-    const value = parseInt(input.value);
-    if (isNaN(value) || value < 0) {
+    const insertedMoney = parseInt(input.value);
+    if (isNaN(insertedMoney) || insertedMoney < 0) {
         return;
     }
 
     const inputAmountDisplay = document.getElementById('inputAmountDisplay');
 
     if(amount > 0) {
-        amount += value;
+        amount += insertedMoney;
     } else {
-        amount = value;
+        amount = insertedMoney;
     }
-    inputAmountDisplay.value = amount;
+    
+    inputAmountDisplay.value = formatAmount(amount);
 }
 
 function purchaseItem(price) {
     console.log(price);
+}
+
+function formatAmount(amount) {
+    return amount.toLocaleString();
 }
