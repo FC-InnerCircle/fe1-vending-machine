5ce3c5dc05cb7f369fde56ecadfa0fe8daed56e7
hyejin choi <chj942@gmail.com>
2024-07-25 05:16:57 +0900

    tailwind 설치

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import "./output.css";
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
   </div>
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import tailwindcss from 'tailwindcss'
+
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  }
+})
