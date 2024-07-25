aede9ee45c0249a47a0fe2348f77caadc42c4101
Geunbaek <rmsqor103@naver.com>
2024-07-25 20:56:36 +0900

    feat: vendingMachine 스토어 추가

---
+++ src/stores/vendingMachine.ts
@@ -0,0 +1,75 @@
+import Store from '../lib/Store';
+
+interface InsertLog {
+  type: 'insert';
+  cost: number;
+}
+
+interface ReturnLog {
+  type: 'return';
+  cost: number;
+}
+
+interface PurchaseLog {
+  type: 'purchase';
+  productName: string;
+}
+
+type Log = InsertLog | ReturnLog | PurchaseLog;
+
+export interface VendingMachineState {
+  totalCost: number;
+  tempCost: number;
+  cost: number;
+  logs: Log[];
+}
+
+interface VendingMachineAction {
+  insertCost: () => void;
+  returnCost: () => void;
+  inputCost: (cost: number) => void;
+  purchase: (productName: string, productCost: number) => void;
+  displayInsufficientCost: (productCost: number) => void;
+}
+
+export const vendingMachineStore = new Store<VendingMachineState>({
+  totalCost: 0,
+  tempCost: 0,
+  cost: 0,
+  logs: [],
+});
+
+export const vendingMachineActions: VendingMachineAction = {
+  insertCost: () => {
+    const { totalCost, cost, logs } = vendingMachineStore.getState();
+    vendingMachineStore.setState({
+      totalCost: totalCost + cost,
+      cost: 0,
+      logs: [...logs, { type: 'insert', cost }],
+    });
+  },
+  returnCost: () => {
+    const { totalCost, logs } = vendingMachineStore.getState();
+    vendingMachineStore.setState({
+      totalCost: 0,
+      logs: [...logs, { type: 'return', cost: totalCost }],
+    });
+  },
+  inputCost: (cost: number) => {
+    vendingMachineStore.setState({
+      cost,
+    });
+  },
+  purchase: (productName: string, productCost: number) => {
+    const { totalCost, logs } = vendingMachineStore.getState();
+    vendingMachineStore.setState({
+      totalCost: totalCost - productCost,
+      logs: [...logs, { type: 'purchase', productName }],
+    });
+  },
+  displayInsufficientCost: (productCost: number) => {
+    vendingMachineStore.setState({
+      tempCost: productCost,
+    });
+  },
+};
