6c2df2079cfed0f84d2339412de77f62d0267cdf
DahnBee Shim <colleksql@8snippet.com>
2024-07-25 21:22:41 +0900

    refactor: Dom element 설정 상수 추가

--- main.js
+++ main.js
@@ -3,20 +3,32 @@ import { initializeBalance, updateBalance, insertMoney, returnMoney } from './mo
 import { initializeItems } from './modules/items.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const MAX_INPUT_AMOUNT = 100000;
+
   const moneyInput = document.getElementById('money-input');
   const balanceDisplay = document.getElementById('balance-display');
+  const insertMoneyBtn = document.getElementById('insert-money');
+  const returnMoneyBtn = document.getElementById('return-money');
 
   initializeBalance(balanceDisplay);
-
   loadFromLocalStorage();
-  
   initializeItems();
   
   moneyInput.addEventListener('input', function() {
     this.value = this.value.replace(/[^0-9]/g, '');
+  
+    if (parseInt(this.value, 10) > MAX_INPUT_AMOUNT) {
+      this.value = MAX_INPUT_AMOUNT;
+    }
   });
 
-  document.getElementById('insert-money').addEventListener('click', () => {
+  moneyInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      document.getElementById('insert-money').click();
+    }
+  });
+
+  insertMoneyBtn.addEventListener('click', () => {
     const input = moneyInput.value;
     const amount = parseInt(input);
     if (amount > 0) {
@@ -26,14 +38,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  document.getElementById('return-money').addEventListener('click', () => {
+  returnMoneyBtn.addEventListener('click', () => {
     returnMoney();
     saveToLocalStorage();
-  });
-
-  moneyInput.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-      document.getElementById('insert-money').click();
-    }
   });
 });
