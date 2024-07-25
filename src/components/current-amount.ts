bb56278488b1f74d296ddc24142a9232f40457f5
AyoungWon <wayinwayne@gmail.com>
2024-07-25 11:08:42 +0900

    refac: 3자릿수 , 찍히는 로직을 intl에서 toLocaleString으로 간소화

--- src/components/current-amount.ts
+++ src/components/current-amount.ts
@@ -3,16 +3,10 @@ const INIT_VALUE = "0";
 export class CurrentAmountInput {
   private inputElement: HTMLInputElement;
   private dataValue: number;
-  private formatter = new Intl.NumberFormat("en", {
-    style: "currency",
-    currency: "krw",
-    maximumSignificantDigits: 21,
-  });
+
   private updateValue(value: number) {
     this.dataValue = value;
-    this.inputElement.value = this.formatter
-      .format(this.dataValue)
-      .toLowerCase();
+    this.inputElement.value = value.toLocaleString();
   }
 
   constructor() {
@@ -45,7 +39,7 @@ export class CurrentAmountInput {
   alertInsufficientAmount(value: number) {
     const currentValue = this.inputElement.value;
     //필요한 금액 알리기
-    this.inputElement.value = this.formatter.format(value).toLowerCase();
+    this.inputElement.value = value.toLocaleString();
 
     setTimeout(() => {
       //원래 금액으로
