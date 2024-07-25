1e08b1421c49f2918e1c3c1ba850b6a11cf21621
이호찬 <hochan.lee@brobotics.kr>
2024-07-25 19:54:27 +0900

    [refactor] totalAmount 초기값 셋팅 리팩토링

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import './styles/style.css'
 import { createProducts, createProductButtons } from './src/services/productService.js';
-import { setupEventListeners } from './src/services/uiService.js';
+import { setupEventListeners, updateTotalAmount } from './src/services/uiService.js';
 
 const products = createProducts(9, 100, 300);
 createProductButtons(products);
 setupEventListeners();
+
+updateTotalAmount(0);
