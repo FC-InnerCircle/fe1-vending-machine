cff6d3d8d5afd31cb2a7043054be884bc96bde2c
jangwonyoon <wkddnjs7310@naver.com>
2024-07-25 15:52:58 +0900

    feat: 이벤트 핸들러 추가
    * 반환 핸들러, 투입 핸들러, 주문 핸들러

---
+++ src/js/contants.js
@@ -0,0 +1,40 @@
+const Items = [
+  {
+    name: 'FE 300',
+    price: 300,
+  },
+  {
+    name: 'FE 400',
+    price: 400,
+  },
+  {
+    name: 'FE 500',
+    price: 500,
+  },
+  {
+    name: 'FE 600',
+    price: 600,
+  },
+  {
+    name: 'FE 700',
+    price: 700,
+  },
+  {
+    name: 'FE 800',
+    price: 700,
+  },
+  {
+    name: 'FE 900',
+    price: 900,
+  },
+  {
+    name: 'FE 1000',
+    price: 1000,
+  },
+  {
+    name: 'FE 1100',
+    price: 1100,
+  },
+];
+
+export default Items;
---
+++ src/js/eventHandlers.js
@@ -0,0 +1,68 @@
+import { returnMoney, insertMoney, buyItem } from './util.js';
+
+export const handleReturnButtonClick = (
+  event,
+  totalBalanceView,
+  transactionLog,
+  transactionLogContainer,
+) => {
+  event.preventDefault();
+
+  const totalBalance = totalBalanceView.value;
+  returnMoney(
+    totalBalance,
+    totalBalanceView,
+    transactionLog,
+    transactionLogContainer,
+  );
+};
+
+export const handleInsertButtonClick = (
+  event,
+  insertCoinView,
+  totalBalanceView,
+  transactionLog,
+  transactionLogContainer,
+) => {
+  event.preventDefault();
+  const coin = insertCoinView.value;
+
+  if (coin > 0) {
+    insertMoney(
+      coin,
+      totalBalanceView,
+      insertCoinView,
+      transactionLog,
+      transactionLogContainer,
+    );
+  } else {
+    if (coin < 0) {
+      alert('음수는 투입할 수 없습니다.');
+    }
+    insertCoinView.value = 0;
+  }
+};
+
+export const handleOrderButtonClick = (
+  event,
+  Items,
+  totalBalanceView,
+  transactionLog,
+  transactionLogContainer,
+) => {
+  event.preventDefault();
+
+  const itemName = event.target.textContent;
+  const item = Items.find((i) => i.name === itemName);
+
+  if (item) {
+    buyItem(
+      item.name,
+      item.price,
+      totalBalanceView,
+      Items,
+      transactionLog,
+      transactionLogContainer,
+    );
+  }
+};
