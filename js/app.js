e267c2653cf0451269b8a849964e5fb49726426f
kmoh <kyomin0306@gmail.com>
2024-07-26 09:47:10 +0900

    feat: Refactor vending machine code

--- js/app.js
+++ js/app.js
@@ -1,58 +1,48 @@
 const CHEAPEST_PRICE = 300;
 let amount = 0;
 
-function insertMoney() {
+const insertMoney = () => {
     const insertedMoney = parseInt(input.value);
 
     if (isNaN(insertedMoney) || insertedMoney < 0) {
         return;
     }
 
-    if(amount > 0) {
-        amount += insertedMoney;
-    } else {
-        amount = insertedMoney;
-    }
-    
+    amount += insertedMoney;
     inputAmountDisplay.value = formatAmount(amount);
     logVendingMachine(insertedMoney, "inserted");
-}
+};
 
-function returnMoney () {
-    if(amount > 0) {
+const returnMoney = () => {
+    if (amount > 0) {
         amount = 0;
-
         logVendingMachine(inputAmountDisplay.value, "returned");
         inputAmountDisplay.value = formatAmount(amount);
     }
-}
+};
 
-function purchaseItem(price) {
-    if(amount >= price) {
+const purchaseItem = (price) => {
+    if (amount >= price) {
         amount -= price;
         inputAmountDisplay.value = formatAmount(amount);
         logVendingMachine(price, "purchased");
 
-        if(amount < CHEAPEST_PRICE) {
+        if (amount < CHEAPEST_PRICE) {
             returnMoney();
         }
     }
-}
+};
 
-function formatAmount(amount) {
+const formatAmount = (amount) => {
     return amount.toLocaleString();
-}
+};
 
-function logVendingMachine(item, action) {
-    if(action === "returned") {
-        output.value += formatAmount(item) + '원을 반환합니다. '  + '\n';
-        return;
-    } else if(action === "purchased") {
-        output.value += item + '을 구매했습니다. '  + '\n';
-
-        return;
-    } else if(action === "inserted") { 
-        output.value += formatAmount(item) + '원을 투입했습니다. '  + '\n';
-        return;
+const logVendingMachine = (item, action) => {
+    if (action === "returned") {
+        output.value += formatAmount(item) + '원을 반환합니다. ' + '\n';
+    } else if (action === "purchased") {
+        output.value += item + '을 구매했습니다. ' + '\n';
+    } else if (action === "inserted") {
+        output.value += formatAmount(item) + '원을 투입했습니다. ' + '\n';
     }
-}
+};
