b7f3615f78c7a0334942cbe6d3c5bf145055b21a
kichul7493 <kichul7493@gmail.com>
2024-07-26 10:42:41 +0900

    fix: 버튼 클릭 반응 추가를 위해 hover, active 배경색 변경

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,10 @@ interface ButtonProps {
 
 const Button = ({ onClick, children }: ButtonProps) => {
   return (
-    <button className="w-[15%] h-10 border-2 border-black" onClick={onClick}>
+    <button
+      className="w-[15%] h-10 border-2 border-black hover:bg-slate-300 active:bg-slate-400"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
