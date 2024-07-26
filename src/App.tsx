5b5fdf000520dbc5b0662b9aa1669c196f8e72c0
kichul7493 <kichul7493@gmail.com>
2024-07-26 10:25:09 +0900

    feat: 레이아웃 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import BalanceController from "./containers/BalanceControlContainer/BalanceControlContainer";
+import BalanceDisplayContainer from "./containers/BalanceDisplayContainer/BalanceDisplayContainer";
+import HistoryContainer from "./containers/HistoryContainer/HistoryContainer";
+import ProductButtonConainer from "./containers/ProductButtonConainer/ProductButtonConainer";
+
 function App() {
   return (
-    <main>
-      <h1 className="text-red-500">Hello, World</h1>
-    </main>
+    <div className="w-full h-screen flex justify-center items-center">
+      <main className="w-full h-full md:h-[400px] flex flex-col md:max-w-3xl md:flex-row">
+        <section className="w-full h-1/2 p-6 bg-slate-200 border-0 md:border-2 border-black md:h-full md:w-1/2">
+          <BalanceDisplayContainer />
+          <ProductButtonConainer />
+        </section>
+        <section className="w-full h-1/2 p-6 flex flex-col gap-6 md:h-full md:w-1/2">
+          <BalanceController />
+          <HistoryContainer />
+        </section>
+      </main>
+    </div>
   );
 }
 
