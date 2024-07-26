3e5a1061aee2fcb02ca38dee86929cb34499df98
kichul7493 <kichul7493@gmail.com>
2024-07-26 10:21:44 +0900

    feat: 자판기 context provider 연결

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import VendingMachineProvider from "./providers/VendingMachineProvider.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <VendingMachineProvider>
+      <App />
+    </VendingMachineProvider>
   </React.StrictMode>
 );
