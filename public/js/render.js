6382d1a9156d210635a87d5eb667d87a6f696fcd
wpdbsx <wpdbsx@naver.com>
2024-07-27 09:12:24 +0900

    fix 최소가 금액 반환 로직 수정

--- public/js/render.js
+++ public/js/render.js
@@ -29,8 +29,11 @@ const render = (function () {
                             textLog: [...state.textLog,
                             `물건(${item.text})를 구매하셨습니다. 구매금액(${item.price}) 남은금액(${state.price - item.price})`]
                         });
-                    } else if (state.price !== 0 && state.price < state.minPrice) {
-                        setState({ price: 0, textLog: [...state.textLog, `최소물건가격(${state.minPrice})보다 현재금액(${state.price})이 부족합니다. 잔돈을 반환합니다.`] })
+                        if (state.price - item.price < state.minPrice) {
+                            setState({ price: 0, textLog: [...state.textLog, `최소물건가격(${state.minPrice})보다 현재남은금액(${state.price})이 부족합니다. 잔돈을 반환합니다.`] })
+
+                        }
+
                     }
 
                 }
